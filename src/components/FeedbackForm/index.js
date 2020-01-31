7b7fc10366ba1e64856a5234edd4a99a3536607c
Zixing Li <pokemon.masters.stuff@gmail.com>
2020-01-31 14:42:50 -0500

    update feedback form width and margin

--- src/components/FeedbackForm/index.js
+++ src/components/FeedbackForm/index.js
@@ -24,10 +24,7 @@ export default class MyForm extends React.Component {
         aria-labelledby="feedbackModalCenterTitle"
         aria-hidden="true"
       >
-        <div
-          className="modal-dialog modal-dialog-centered mw-50 w-50"
-          role="document"
-        >
+        <div className="modal-dialog mt-5 w-50" role="document">
           <div className="modal-content">
             <div className="modal-header">
               <h5 className="modal-title" id="feedBackModalLongTitle">
