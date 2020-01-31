f791988c2688b2660b0e4e1169b213a585abea58
Zixing Li <pokemon.masters.stuff@gmail.com>
2020-01-31 16:38:19 -0500

    Updates

---
+++ precache-manifest.1516cf09874ad76451947ee7f2b538c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fc33c05aef24967828558e5ad7e2d64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4597bb592c2b6766fbd0",
+    "url": "/static/css/main.36add569.chunk.css"
+  },
+  {
+    "revision": "c83c2e1182ebc0479ee5",
+    "url": "/static/js/2.58751b9f.chunk.js"
+  },
+  {
+    "revision": "4597bb592c2b6766fbd0",
+    "url": "/static/js/main.c9ab6674.chunk.js"
+  },
+  {
+    "revision": "d01a8f8dd223bfbd43cc",
+    "url": "/static/js/runtime-main.ecbf40b8.js"
+  }
+]);
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import pokemonReducer from './pokemonReducer';
-import gridReducer from './gridReducer';
-
-export default combineReducers({
-  pokemon: pokemonReducer,
-  grid: gridReducer
-});
