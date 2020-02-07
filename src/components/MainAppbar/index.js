3a5a754c24dc504742d87f8120258feb0cdad784
Zixing Li <pokemon.masters.stuff@gmail.com>
2020-02-07 18:29:08 -0500

    allows remaining energy to go negative so users are aware

--- src/components/MainAppbar/index.js
+++ src/components/MainAppbar/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import {withStyles} from '@material-ui/core';
+import { withStyles } from '@material-ui/core';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Typography from '@material-ui/core/Typography';
 import AppBar from '@material-ui/core/AppBar';
 
-import styles from "./styles";
+import styles from './styles';
 
 function MainAppbar(props) {
-  const {classes, onOpenNavHandler, data} = props;
+  const { classes, onOpenNavHandler, data } = props;
 
-  const handleOnOpenNav = () => (typeof onOpenNavHandler === "function") ? onOpenNavHandler() : null;
+  const handleOnOpenNav = () =>
+    typeof onOpenNavHandler === 'function' ? onOpenNavHandler() : null;
 
   return (
     <AppBar position="fixed">
@@ -23,7 +24,7 @@ function MainAppbar(props) {
           aria-label="menu"
           onClick={handleOnOpenNav}
         >
-          <MenuIcon/>
+          <MenuIcon />
         </IconButton>
         <Typography variant="h6" className={classes.title}>
           Sync Grid Helper
@@ -32,7 +33,7 @@ function MainAppbar(props) {
 
       <Toolbar className={classes.subToolbar}>
         <Typography variant="body1" className={classes.title}>
-          Remaining Energy: {Boolean(data && data.energy > -1) && data.energy}
+          Remaining Energy: {data.energy}
         </Typography>
         <Typography variant="body1" className={classes.title}>
           Orbs Spent: {Boolean(data && data.orbs > -1) && data.orbs}
