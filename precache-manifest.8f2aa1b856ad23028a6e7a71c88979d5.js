3dc3e556d35a9c821c4fcef30bb0fffde2fbbf74
Zixing Li <pokemon.masters.stuff@gmail.com>
2020-02-15 17:06:01 -0500

    Updates

--- precache-manifest.8f2aa1b856ad23028a6e7a71c88979d5.js
+++ precache-manifest.8f2aa1b856ad23028a6e7a71c88979d5.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "24e565a447884a30f3ab07cca7d41807",
+    "revision": "51c7c217bb6adc96fec5da99f96b67a0",
     "url": "/index.html"
   },
   {
-    "revision": "938d0a62d79235c99b2e",
+    "revision": "f7b88da7cddb4a31f537",
     "url": "/static/css/main.07e39449.chunk.css"
   },
   {
-    "revision": "551f0a964fd0a61e2ffd",
-    "url": "/static/js/2.1d136e3c.chunk.js"
+    "revision": "3f15a0c1fd10ffc29f0c",
+    "url": "/static/js/2.fdf3e2db.chunk.js"
   },
   {
-    "revision": "938d0a62d79235c99b2e",
-    "url": "/static/js/main.0c432a14.chunk.js"
+    "revision": "f7b88da7cddb4a31f537",
+    "url": "/static/js/main.e0ac6f84.chunk.js"
   },
   {
     "revision": "d01a8f8dd223bfbd43cc",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3a53f26755b7c66f7143d05df98059bd.js"
+  "/precache-manifest.8f2aa1b856ad23028a6e7a71c88979d5.js"
 );
 
 self.addEventListener('message', (event) => {
