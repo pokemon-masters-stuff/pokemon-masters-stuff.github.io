64912dcb95278638146f6b94e5fce2ea495b9f23
Jonathan Gervais <jgervais@baselinetelematics.com>
2020-02-16 07:17:31 -0500

    - Added `monsterId` to list of pokemons for possible future use

--- src/data/pokemonList.js
+++ src/data/pokemonList.js
@@ -1,21 +1,22 @@
 let list = [
-  { name: 'Pikachu' },
-  { name: 'Torkoal' },
-  { name: 'Infernape' },
-  { name: 'Dewgong' },
-  { name: 'Haxorus' },
-  { name: 'Kingdra' },
-  { name: 'Serperior' },
-  { name: 'Vileplume' },
-  { name: 'Mew' },
-  { name: 'Metagross' }
+  { name: 'Pikachu', id: 20002500 },
+  { name: 'Torkoal', id: 20032401 },
+  { name: 'Infernape', id: 20039200 },
+  { name: 'Dewgong', id: 20008700 },
+  { name: 'Haxorus', id: 20061201 },
+  { name: 'Kingdra', id: 20038400 },
+  { name: 'Serperior', id: 20049701 },
+  { name: 'Vileplume', id: 20004501 },
+  { name: 'Mew', id: 20015111},
+  { name: 'Metagross', id: 20037600 }
 ];
 
 let pokemonList = list.map((obj, index) => {
   return {
     ...obj,
     key: index,
-    value: obj.name
+    value: obj.name,
+    id: obj.id,
   };
 });
 
