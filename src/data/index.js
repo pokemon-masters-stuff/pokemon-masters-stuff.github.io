69bb2916695da40ac43035d8bffd885652fad2e9
Jonathan Gervais <jgervais@baselinetelematics.com>
2020-02-17 15:11:17 -0500

    - Added updated json db for Haxorus - Added entries in `shortenedMoveNameByCellId.js` for Haxorus's grid

--- src/data/index.js
+++ src/data/index.js
@@ -3,7 +3,7 @@ export { default as pikachuGridData } from './grids/pikachu';
 export { default as torkoalGridData } from './grids/torkoal.json';
 export { default as infernapeGridData } from './grids/infernape.json';
 export { default as dewgongGridData } from './grids/dewgong.json';
-export { default as haxorusGridData } from './grids/haxorus';
+export { default as haxorusGridData } from './grids/haxorus.json';
 export { default as kingdraGridData } from './grids/kingdra.json';
 export { default as serperiorGridData } from './grids/serperior';
 export { default as vileplumeGridData } from './grids/vileplume.json';
