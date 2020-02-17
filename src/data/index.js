48dd8b5455cb7e94d1c07a0204d9fc5debc16d27
Jonathan Gervais <jgervais@baselinetelematics.com>
2020-02-17 14:35:05 -0500

    - Added updated json db for Infernape - Added updated json db for Kingdra - Added updated json db for Vileplume - Added entries in `shortenedMoveNameByCellId.js` for Infernape, Kingdra, Vileplume grids

--- src/data/index.js
+++ src/data/index.js
@@ -1,12 +1,12 @@
 export { default as pokemonList } from './pokemonList';
 export { default as pikachuGridData } from './grids/pikachu';
 export { default as torkoalGridData } from './grids/torkoal';
-export { default as infernapeGridData } from './grids/infernape';
+export { default as infernapeGridData } from './grids/infernape.json';
 export { default as dewgongGridData } from './grids/dewgong.json';
 export { default as haxorusGridData } from './grids/haxorus';
-export { default as kingdraGridData } from './grids/kingdra';
+export { default as kingdraGridData } from './grids/kingdra.json';
 export { default as serperiorGridData } from './grids/serperior';
-export { default as vileplumeGridData } from './grids/vileplume';
+export { default as vileplumeGridData } from './grids/vileplume.json';
 export { default as mewGridData } from './grids/mew';
 export { default as metagrossGridData } from './grids/metagross';
 export { default as shortenedMoveNameByCellId } from './shortenedMoveNameByCellId';
