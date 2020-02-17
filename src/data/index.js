778cd3135e3bce5f5224f50e6f7d081f157b3405
Jonathan Gervais <jgervais@baselinetelematics.com>
2020-02-17 15:59:56 -0500

    - Added updated json db for Pikachu - Added entries in `shortenedMoveNameByCellId.js` for Pikachu's grid

--- src/data/index.js
+++ src/data/index.js
@@ -1,5 +1,5 @@
 export { default as pokemonList } from './pokemonList';
-export { default as pikachuGridData } from './grids/pikachu';
+export { default as pikachuGridData } from './grids/pikachu.json';
 export { default as torkoalGridData } from './grids/torkoal.json';
 export { default as infernapeGridData } from './grids/infernape.json';
 export { default as dewgongGridData } from './grids/dewgong.json';
