8344c496f19004710b2e26f8151e979b4f35d3f0
Zixing Li <pokemon.masters.stuff@gmail.com>
2020-02-19 13:40:10 -0500

    Added pokemon pictures for all sync grids except for mews. Modified GridMap render to only show text when picture is not available

--- src/images/PokemonSvgLink/index.js
+++ src/images/PokemonSvgLink/index.js
@@ -1,1 +1,9 @@
 export { default as pikachuSvgLink } from './pikachu.js';
+export { default as torkoalSvgLink } from './torkoal.js';
+export { default as serperiorSvgLink } from './serperior.js';
+export { default as kingdraSvgLink } from './kingdra.js';
+export { default as vileplumeSvgLink } from './vileplume.js';
+export { default as haxorusSvgLink } from './haxorus.js';
+export { default as infernapeSvgLink } from './infernape.js';
+export { default as dewgongSvgLink } from './dewgong.js';
+export { default as metagrossSvgLink } from './metagross.js';
