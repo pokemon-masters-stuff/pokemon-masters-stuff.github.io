c5c44d6276401800707effe8e017751262faa5d8
Zixing Li <pokemon.masters.stuff@gmail.com>
2020-02-20 17:53:07 -0500

    Added SvgLinks for all sync pairs datamined on 2.20

--- src/images/PokemonSvgLink/index.js
+++ src/images/PokemonSvgLink/index.js
@@ -8,3 +8,11 @@ export { default as infernapeSvgLink } from './infernape.js';
 export { default as dewgongSvgLink } from './dewgong.js';
 export { default as mewSvgLink } from './mew.js';
 export { default as metagrossSvgLink } from './metagross.js';
+
+export { default as charizardSvgLink } from './charizard.js';
+export { default as alakazamSvgLink } from './alakazam.js';
+export { default as houndoomSvgLink } from './houndoom.js';
+export { default as liepardSvgLink } from './liepard.js';
+export { default as palossandSvgLink } from './palossand.js';
+export { default as raichuSvgLink } from './raichu.js';
+export { default as rotomSvgLink } from './rotom.js';
