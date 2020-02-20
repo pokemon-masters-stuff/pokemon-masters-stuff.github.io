ee2d9a996646dd7b9174e569c682574026b6bd78
Zixing Li <pokemon.masters.stuff@gmail.com>
2020-02-20 17:39:51 -0500

    Resolved conflicts in PokemonSvgLink during merge

--- src/images/PokemonSvgLink/index.js
+++ src/images/PokemonSvgLink/index.js
@@ -1,11 +1,4 @@
 export { default as pikachuSvgLink } from './pikachu.js';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> Added pokemon pictures for all sync grids except for mews. Modified GridMap render to only show text when picture is not available
-=======
->>>>>>> a97cfe61f2dddb5443d4c278b3387b6ecdb953ba
 export { default as torkoalSvgLink } from './torkoal.js';
 export { default as serperiorSvgLink } from './serperior.js';
 export { default as kingdraSvgLink } from './kingdra.js';
@@ -13,14 +6,5 @@ export { default as vileplumeSvgLink } from './vileplume.js';
 export { default as haxorusSvgLink } from './haxorus.js';
 export { default as infernapeSvgLink } from './infernape.js';
 export { default as dewgongSvgLink } from './dewgong.js';
-<<<<<<< HEAD
 export { default as mewSvgLink } from './mew.js';
 export { default as metagrossSvgLink } from './metagross.js';
-<<<<<<< HEAD
-=======
->>>>>>> Added POC for Pikachu, showing icon in center grid
-=======
->>>>>>> Added pokemon pictures for all sync grids except for mews. Modified GridMap render to only show text when picture is not available
-=======
-export { default as metagrossSvgLink } from './metagross.js';
->>>>>>> a97cfe61f2dddb5443d4c278b3387b6ecdb953ba
