f0c2ef41a8ecc2f965c9fe06cc838acd4fe91342
Zixing Li <pokemon.masters.stuff@gmail.com>
2020-02-23 10:16:57 -0500

    Added queryString and related hook and functions

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
-import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
 import MobileLayout from './containers/MobileLayout';
 import DesktopLayout from './containers/DesktopLayout';
@@ -23,7 +23,7 @@ const persistConfig = {
   key: 'root',
   version: 0,
   storage,
-  stateReconciler: autoMergeLevel2,
+  stateReconciler: autoMergeLevel2
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
