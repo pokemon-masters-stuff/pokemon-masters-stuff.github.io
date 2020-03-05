c27afc0d59b48364abd80acf18ab72f154b03df3
Zixing Li <pokemon.masters.stuff@gmail.com>
2020-03-04 19:34:29 -0500

    Cleaned up some spaces in Navigation

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -6,9 +6,7 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import FeedbackIcon from '@material-ui/icons/Feedback';
-
 import { FeedbackFormMobile } from '../FeedbackForm';
-
 import styles from './styles';
 
 function Navigation(props) {
