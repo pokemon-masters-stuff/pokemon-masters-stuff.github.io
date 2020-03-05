2128998cb935b0fb4d87e7fc51e6249ab7d0265e
Zixing Li <pokemon.masters.stuff@gmail.com>
2020-03-04 19:55:18 -0500

    Refactored Desktops SaveBuildButton to use functional component and hooks

--- src/components/SaveBuildButton/Desktop.js
+++ src/components/SaveBuildButton/Desktop.js
@@ -1,111 +1,102 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+// import React, { Component } from 'react';
+import React, { Fragment, useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import {
   saveCurrentBuild,
   deleteSelectedBuild
 } from '../../actions/actionCreators';
 import './desktop.css';
 
-class SaveBuildButton extends Component {
-  constructor(props) {
-    super(props);
-    this.newBuildNameRef = React.createRef();
-  }
+export default function SaveBuildButton() {
+  const dispatch = useDispatch();
+  const pokemon = useSelector(state => state.pokemon);
+  const savedBuilds = useSelector(state =>
+    state.grid.savedBuilds.allIds.map(id => state.grid.savedBuilds.byIds[id])
+  );
+  const newBuildNameRef = useRef(null);
 
-  handleOnSaveBuild = () => {
+  const handleOnSaveBuild = () => {
     let userConfirmation = true;
-    if (this.newBuildNameRef.current.value) {
-      // If already has a save with the same name, delete old save
-      for (let build in this.props.savedBuilds) {
+    if (newBuildNameRef.current.value) {
+      // If already have a save with the same name, delete old save
+      for (let build in savedBuilds) {
         if (
-          this.props.savedBuilds[build].name ===
-            this.newBuildNameRef.current.value &&
-          this.props.savedBuilds[build].pokemon ===
-            this.props.pokemon.selectedPokemon
+          savedBuilds[build].name === newBuildNameRef.current.value &&
+          savedBuilds[build].pokemon === pokemon.selectedPokemon
         ) {
           userConfirmation = window.confirm(
             'There is a save with the same name. Do you wish to overwrite it?'
           );
           userConfirmation &&
-            this.props.deleteSelectedBuild({
-              buildId: this.props.savedBuilds[build].id
-            });
+            dispatch(
+              deleteSelectedBuild({
+                buildId: savedBuilds[build].id
+              })
+            );
         }
       }
       userConfirmation &&
-        this.props.saveCurrentBuild({
-          selectedPokemon: this.props.pokemon.selectedPokemon,
-          buildName: this.newBuildNameRef.current.value
-        });
+        dispatch(
+          saveCurrentBuild({
+            selectedPokemon: pokemon.selectedPokemon,
+            buildName: newBuildNameRef.current.value
+          })
+        );
     } else alert('Please enter a name');
   };
 
-  render() {
-    return (
-      <div>
-        <button
-          type="button"
-          className="btn btn-primary"
-          data-toggle="modal"
-          data-target="#saveBuildModal"
-          style={{ position: 'relative', zIndex: 999 }}
-        >
-          Save Build
-        </button>
+  return (
+    <Fragment>
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-toggle="modal"
+        data-target="#saveBuildModal"
+        style={{ position: 'relative', zIndex: 999 }}
+      >
+        Save Build
+      </button>
 
-        <div
-          className="modal fade"
-          id="saveBuildModal"
-          tabIndex="-1"
-          role="dialog"
-          aria-labelledby="myModalLabel"
-          aria-hidden="true"
-        >
-          <div className="modal-dialog modal-dialog-centered" role="document">
-            <div className="modal-content">
-              <div className="modal-header text-center">
-                <h4 className="modal-title w-100 font-weight-bold">
-                  Save a new build
-                </h4>
+      <div
+        className="modal fade"
+        id="saveBuildModal"
+        tabIndex="-1"
+        role="dialog"
+        aria-labelledby="myModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered" role="document">
+          <div className="modal-content">
+            <div className="modal-header text-center">
+              <h4 className="modal-title w-100 font-weight-bold">
+                Save a new build
+              </h4>
+            </div>
+            <div className="modal-body mx-3">
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="save"
+                  placeholder="Build name"
+                  key={`${Math.floor(Math.random() * 1000)}-min`}
+                  defaultValue=""
+                  ref={newBuildNameRef}
+                />
               </div>
-              <div className="modal-body mx-3">
-                <div className="form-group">
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="save"
-                    placeholder="Build name"
-                    key={`${Math.floor(Math.random() * 1000)}-min`}
-                    defaultValue=""
-                    ref={this.newBuildNameRef}
-                  />
-                </div>
-              </div>
-              <div className="modal-footer d-flex justify-content-center">
-                <button
-                  className="btn btn-default"
-                  onClick={this.handleOnSaveBuild}
-                  data-dismiss="modal"
-                >
-                  Save
-                </button>
-              </div>
+            </div>
+            <div className="modal-footer d-flex justify-content-center">
+              <button
+                className="btn btn-default"
+                onClick={handleOnSaveBuild}
+                data-dismiss="modal"
+              >
+                Save
+              </button>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </Fragment>
+  );
 }
-const mapStateToProps = state => ({
-  pokemon: state.pokemon,
-  grid: state.grid,
-  savedBuilds: state.grid.savedBuilds.allIds.map(
-    id => state.grid.savedBuilds.byIds[id]
-  )
-});
-
-export default connect(mapStateToProps, {
-  saveCurrentBuild,
-  deleteSelectedBuild
-})(SaveBuildButton);
