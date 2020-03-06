ea6a8ce15fe7fdf24685f24f336bfc334e203df6
Zixing Li <li.zx.luke@gmail.com>
2020-03-06 14:20:42 -0500

    Updated Reset buttons to update url when clicked on

--- src/components/ResetGridButton/Desktop.js
+++ src/components/ResetGridButton/Desktop.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { resetGrids } from '../../actions/actionCreators';
+import { useSelector, useDispatch } from 'react-redux';
+import { resetGrids, updateUrl } from '../../actions/actionCreators';
 
 const ResetGridButton = () => {
   const dispatch = useDispatch();
+  const pokemon = useSelector(state => state.pokemon.selectedPokemon);
 
   const handleOnClick = () => {
     dispatch(resetGrids());
+    dispatch(updateUrl(pokemon));
   };
 
   return (
--- src/components/ResetGridButton/Mobile.js
+++ src/components/ResetGridButton/Mobile.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import { useDispatch } from 'react-redux';
-import { resetGrids } from '../../actions/actionCreators';
+import { useSelector, useDispatch } from 'react-redux';
+import { resetGrids, updateUrl } from '../../actions/actionCreators';
 
 const ResetGridButton = () => {
   const dispatch = useDispatch();
+  const pokemon = useSelector(state => state.pokemon.selectedPokemon);
 
   const handleOnClick = () => {
     dispatch(resetGrids());
+    dispatch(updateUrl(pokemon));
   };
 
   return (
