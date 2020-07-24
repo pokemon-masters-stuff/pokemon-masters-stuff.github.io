2dc023d1a5131cfd4bad6f1ddc8a6b64bca9069e
Zixing Li <li.zx.luke@gmail.com>
2020-07-24 14:13:05 -0400

    Made calculator's container smaller

--- src/components/GachaOddsCalculator/Desktop.js
+++ src/components/GachaOddsCalculator/Desktop.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
 import TextField from "@material-ui/core/TextField";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import { makeStyles } from "@material-ui/core/styles";
@@ -7,6 +6,7 @@ import Tooltip from "@material-ui/core/Tooltip";
 import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
 import Paper from "@material-ui/core/Paper";
 import Divider from "@material-ui/core/Divider";
+import Container from "@material-ui/core/Container";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -35,7 +35,6 @@ function binomial(n, k, p) {
 
 const GachaOddsCalculator = () => {
   const classes = useStyles();
-  const darkMode = useSelector((state) => state.darkMode.mode);
   const [paidScouts, setPaidScouts] = useState(0);
   const [nonPaidScouts, setNonPaidScouts] = useState(0);
   const [targetRate, setTargetRate] = useState(2);
@@ -71,203 +70,193 @@ const GachaOddsCalculator = () => {
   let nonPaidGems = !isNaN(nonPaidScouts) ? nonPaidScouts * 300 : 0;
 
   return (
-    <div className={`App ${darkMode ? "dark-mode" : null}`}>
-      <div className="content">
-        <div className="container container-s">
-          <Paper width={1} style={{ padding: 50, marginTop: 20 }}>
-            <form className={classes.root}>
-              <h3>Gacha Odds Calculator</h3>
-              <Divider />
-              <br />
-              <h4>Inputs: </h4>
-              <div
-                className="row"
-                style={{ display: "flex", alignItems: "center" }}
-              >
-                <TextField
-                  id="target-unit-rate"
-                  label="Rate of Target Unit"
-                  type="number"
-                  value={targetRate}
-                  onChange={handleChangeFocusRate}
-                  inputProps={{ min: 0 }}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">%</InputAdornment>
-                    ),
-                  }}
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
-                  variant="outlined"
-                  margin="dense"
-                />{" "}
-                <Tooltip
-                  title="2% for focus unit. Check in-game offering rate for non-focus
+    <Container maxWidth="sm" style={{ paddingTop: 10, marginBottom: 30 }}>
+      <Paper width={1} style={{ padding: 50, marginTop: 20 }}>
+        <form className={classes.root}>
+          <h3>Gacha Odds Calculator</h3>
+          <Divider />
+          <br />
+          <h4>Inputs: </h4>
+          <div
+            className="row"
+            style={{ display: "flex", alignItems: "center" }}
+          >
+            <TextField
+              id="target-unit-rate"
+              label="Rate of Target Unit"
+              type="number"
+              value={targetRate}
+              onChange={handleChangeFocusRate}
+              inputProps={{ min: 0 }}
+              InputProps={{
+                endAdornment: <InputAdornment position="end">%</InputAdornment>,
+              }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+              margin="dense"
+            />{" "}
+            <Tooltip
+              title="2% for focus unit. Check in-game offering rate for non-focus
                 units"
-                  placement="top"
-                >
-                  <HelpOutlineIcon />
-                </Tooltip>
-              </div>
-              <div
-                className="row"
-                style={{ display: "flex", alignItems: "center" }}
-              >
-                <TextField
-                  id="five-star-unit-rate"
-                  label="Rate of 5-Star Unit"
-                  type="number"
-                  value={fiveStarRate}
-                  onChange={handleChangeFiveStarRate}
-                  inputProps={{ min: 0 }}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">%</InputAdornment>
-                    ),
-                  }}
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
-                  variant="outlined"
-                  margin="dense"
-                />{" "}
-                <Tooltip
-                  title="7% for Spotlight Scout. 10% for Poke Fair Scout"
-                  placement="top"
-                >
-                  <HelpOutlineIcon />
-                </Tooltip>
-              </div>
-              <div className="row">
-                <TextField
-                  id="paid-scouts"
-                  label="Paid Scouts"
-                  type="number"
-                  value={paidScouts}
-                  onChange={handleChangePaidScouts}
-                  inputProps={{ min: 0 }}
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
-                  variant="outlined"
-                  margin="dense"
-                />
-              </div>
-              <div className="row">
-                <TextField
-                  id="non-paid-scouts"
-                  label="Non-Paid Scouts"
-                  type="number"
-                  value={nonPaidScouts}
-                  onChange={handleChangeNonPaidScouts}
-                  inputProps={{ min: 0 }}
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
-                  variant="outlined"
-                  margin="dense"
-                />
-              </div>
-              <br />
-              <Divider />
-              <br />
-              <h4>Results: </h4>
-              Total number of scouts:{" "}
-              <span style={{ color: "red" }}>{totalScouts}</span>
-              <br />
-              Total gems spent:{" "}
-              <span style={{ color: "red" }}>
-                {paidGems + nonPaidGems}{" "}
-              </span>{" "}
-              (Paid: <span style={{ color: "red" }}> {paidGems}</span>;
-              Non-Paid: <span style={{ color: "red" }}>{nonPaidGems}</span> )
-              <br />
-              <br />
-              Probability of summoning at least 1 target unit:&nbsp; &nbsp;
-              <span style={{ color: "red" }}>
-                {totalScouts === 0
-                  ? 0
-                  : (
-                      (1 - binomial(totalScouts, 0, targetRate / 100)) *
-                      100
-                    ).toFixed(4)}
-                %
-              </span>
-              <br />
-              Probability of summoning at least 2 target units:{" "}
-              <span style={{ color: "red" }}>
-                {totalScouts === 0
-                  ? 0
-                  : (
-                      (1 -
-                        binomial(totalScouts, 0, targetRate / 100) -
-                        binomial(totalScouts, 1, targetRate / 100)) *
-                      100
-                    ).toFixed(4)}
-                %
-              </span>
-              <br />
-              Probability of summoning at least 3 target units:{" "}
-              <span style={{ color: "red" }}>
-                {totalScouts === 0
-                  ? 0
-                  : (
-                      (1 -
-                        binomial(totalScouts, 0, targetRate / 100) -
-                        binomial(totalScouts, 1, targetRate / 100) -
-                        binomial(totalScouts, 2, targetRate / 100)) *
-                      100
-                    ).toFixed(4)}
-                %
-              </span>
-              <br />
-              Probability of summoning at least 4 target units:{" "}
-              <span style={{ color: "red" }}>
-                {totalScouts === 0
-                  ? 0
-                  : (
-                      (1 -
-                        binomial(totalScouts, 0, targetRate / 100) -
-                        binomial(totalScouts, 1, targetRate / 100) -
-                        binomial(totalScouts, 2, targetRate / 100) -
-                        binomial(totalScouts, 3, targetRate / 100)) *
-                      100
-                    ).toFixed(4)}
-                %
-              </span>
-              <br />
-              Probability of summoning at least 5 target units:{" "}
-              <span style={{ color: "red" }}>
-                {totalScouts === 0
-                  ? 0
-                  : (
-                      (1 -
-                        binomial(totalScouts, 0, targetRate / 100) -
-                        binomial(totalScouts, 1, targetRate / 100) -
-                        binomial(totalScouts, 2, targetRate / 100) -
-                        binomial(totalScouts, 3, targetRate / 100) -
-                        binomial(totalScouts, 4, targetRate / 100)) *
-                      100
-                    ).toFixed(4)}
-                %
-              </span>
-              <br />
-              <br />
-              You can expect a total of{" "}
-              <span style={{ color: "red" }}>
-                {Math.floor(
-                  (totalScouts * (!isNaN(fiveStarRate) ? fiveStarRate : 0)) /
-                    100
-                )}{" "}
-              </span>{" "}
-              5-Star units (focus and non-focus).
-              <br />
-            </form>
-          </Paper>
-        </div>
-      </div>
-    </div>
+              placement="top"
+            >
+              <HelpOutlineIcon />
+            </Tooltip>
+          </div>
+          <div
+            className="row"
+            style={{ display: "flex", alignItems: "center" }}
+          >
+            <TextField
+              id="five-star-unit-rate"
+              label="Rate of 5-Star Unit"
+              type="number"
+              value={fiveStarRate}
+              onChange={handleChangeFiveStarRate}
+              inputProps={{ min: 0 }}
+              InputProps={{
+                endAdornment: <InputAdornment position="end">%</InputAdornment>,
+              }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+              margin="dense"
+            />{" "}
+            <Tooltip
+              title="7% for Spotlight Scout. 10% for Poke Fair Scout"
+              placement="top"
+            >
+              <HelpOutlineIcon />
+            </Tooltip>
+          </div>
+          <div className="row">
+            <TextField
+              id="paid-scouts"
+              label="Paid Scouts"
+              type="number"
+              value={paidScouts}
+              onChange={handleChangePaidScouts}
+              inputProps={{ min: 0 }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+              margin="dense"
+            />
+          </div>
+          <div className="row">
+            <TextField
+              id="non-paid-scouts"
+              label="Non-Paid Scouts"
+              type="number"
+              value={nonPaidScouts}
+              onChange={handleChangeNonPaidScouts}
+              inputProps={{ min: 0 }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+              margin="dense"
+            />
+          </div>
+          <br />
+          <Divider />
+          <br />
+          <h4>Results: </h4>
+          Total number of scouts:{" "}
+          <span style={{ color: "red" }}>{totalScouts}</span>
+          <br />
+          Total gems spent:{" "}
+          <span style={{ color: "red" }}>
+            {paidGems + nonPaidGems}{" "}
+          </span> (Paid: <span style={{ color: "red" }}> {paidGems}</span>;
+          Non-Paid: <span style={{ color: "red" }}>{nonPaidGems}</span> )
+          <br />
+          <br />
+          Probability of summoning at least 1 target unit:&nbsp; &nbsp;
+          <span style={{ color: "red" }}>
+            {totalScouts === 0
+              ? 0
+              : (
+                  (1 - binomial(totalScouts, 0, targetRate / 100)) *
+                  100
+                ).toFixed(4)}
+            %
+          </span>
+          <br />
+          Probability of summoning at least 2 target units:{" "}
+          <span style={{ color: "red" }}>
+            {totalScouts === 0
+              ? 0
+              : (
+                  (1 -
+                    binomial(totalScouts, 0, targetRate / 100) -
+                    binomial(totalScouts, 1, targetRate / 100)) *
+                  100
+                ).toFixed(4)}
+            %
+          </span>
+          <br />
+          Probability of summoning at least 3 target units:{" "}
+          <span style={{ color: "red" }}>
+            {totalScouts === 0
+              ? 0
+              : (
+                  (1 -
+                    binomial(totalScouts, 0, targetRate / 100) -
+                    binomial(totalScouts, 1, targetRate / 100) -
+                    binomial(totalScouts, 2, targetRate / 100)) *
+                  100
+                ).toFixed(4)}
+            %
+          </span>
+          <br />
+          Probability of summoning at least 4 target units:{" "}
+          <span style={{ color: "red" }}>
+            {totalScouts === 0
+              ? 0
+              : (
+                  (1 -
+                    binomial(totalScouts, 0, targetRate / 100) -
+                    binomial(totalScouts, 1, targetRate / 100) -
+                    binomial(totalScouts, 2, targetRate / 100) -
+                    binomial(totalScouts, 3, targetRate / 100)) *
+                  100
+                ).toFixed(4)}
+            %
+          </span>
+          <br />
+          Probability of summoning at least 5 target units:{" "}
+          <span style={{ color: "red" }}>
+            {totalScouts === 0
+              ? 0
+              : (
+                  (1 -
+                    binomial(totalScouts, 0, targetRate / 100) -
+                    binomial(totalScouts, 1, targetRate / 100) -
+                    binomial(totalScouts, 2, targetRate / 100) -
+                    binomial(totalScouts, 3, targetRate / 100) -
+                    binomial(totalScouts, 4, targetRate / 100)) *
+                  100
+                ).toFixed(4)}
+            %
+          </span>
+          <br />
+          <br />
+          You can expect a total of{" "}
+          <span style={{ color: "red" }}>
+            {Math.floor(
+              (totalScouts * (!isNaN(fiveStarRate) ? fiveStarRate : 0)) / 100
+            )}{" "}
+          </span>{" "}
+          5-Star units (focus and non-focus).
+          <br />
+        </form>
+      </Paper>
+    </Container>
   );
 };
 
