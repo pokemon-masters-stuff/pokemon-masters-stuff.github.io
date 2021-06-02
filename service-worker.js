7afe50091611aef5cc177935a16a39e7f2d46986
Zixing Li <li.zx.luke@gmail.com>
2021-06-01 20:55:39 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7ed7caf087cdad952f835ef50735dae2.js"
+  "/precache-manifest.eef80afc368229fefbe5cc4239a39fdd.js"
 );
 
 self.addEventListener('message', (event) => {
